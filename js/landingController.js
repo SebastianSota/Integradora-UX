7d582dcad7b820796607e009f311211e3d77b4c7
luis2g <luisjimenez2g@gmail.com>
2021-12-16 17:07:38 -0600

    [TODO]

--- js/landingController.js
+++ js/landingController.js
@@ -21,18 +21,22 @@ myApp.controller("landingController", ($scope) => {
         ],
         information: [
             {
+                id : "mision",
                 title: "Misión",
                 desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
             },
             {
+                id:"vision",
                 title: "Visión",
                 desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
             },
             {
+                id:"valores",
                 title: "Valores",
                 desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
             },
             {
+                id:"contacto",
                 title: "Contáctanos",
                 desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
             },
